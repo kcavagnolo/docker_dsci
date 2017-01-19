506f0b01c5f3214ae4b4826df038c2af1147f201
Ken Cavagnolo <ken@kcavagnolo.com>
2017-01-19 15:29:28 -0500

    Updated notes

--- dsci_services.sh
+++ dsci_services.sh
@@ -5,4 +5,4 @@ set -x
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0 &
 /usr/bin/shiny-server &
 java -jar /opt/h2o.jar &
-jupyter notebook --ip=0.0.0.0 --no-browser
+jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser
